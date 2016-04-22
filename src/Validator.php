f5243e535855e51a70e8fcf09a6eb2b6dad299ff
Ivan Bozhanov <git@vakata.com>
2016-04-22 19:20:46 +0300

    added all remaining untested validation rules

--- src/Validator.php
+++ src/Validator.php
@@ -497,4 +497,327 @@ class Validator
             return filter_var($value, FILTER_VALIDATE_IP) !== false;
         }, $message);
     }
+    /**
+     * Add an URL validation
+     * @method url
+     * @param  array|null $protocols array of allowed protocols (defaults to ['http','https'])
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function url($protocols = null, $message = '')
+    {
+        if (!is_array($protocols)) {
+            $protocols = [ 'http', 'https' ];
+        }
+        return $this->callback(function ($value, $data) use ($protocols) {
+            return filter_var($value, FILTER_VALIDATE_URL) !== false &&
+                   in_array(parse_url($value, PHP_URL_SCHEME), $protocols);
+        }, $message);
+    }
+    /**
+     * Add a mod10 validation
+     * @method mod10
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function mod10($message)
+    {
+        return $this->callback(function ($value, $data) {
+            if (!ctype_digit($value)) {
+                return false;
+            }
+            $value = array_reverse(str_split($value));
+            foreach ($value as $k => $digit) {
+                $value[$k] = $digit ? (($digit * ($k % 2 ? 2 : 1)) % 9 ?: 9) : 0;
+            }
+            return array_sum($value) % 10 === 0;
+        }, $message);
+    }
+    /**
+     * Add a imei validation
+     * @method imei
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function imei($message)
+    {
+        return $this->callback(function ($value, $data) {
+            $value = preg_replace('(\D)', '', $value);
+            $value = array_reverse(str_split($value));
+            foreach ($value as $k => $digit) {
+                $value[$k] = $digit ? (($digit * ($k % 2 ? 2 : 1)) % 9 ?: 9) : 0;
+            }
+            return array_sum($value) % 10 === 0;
+        }, $message);
+    }
+    /**
+     * Add credit card validation
+     * @method creditcard
+     * @param  array|null $types   optional array of allowed cards (visa, mastercard, americanexpress, dinersclub, discover, jcb)
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function creditcard(array $types = null, $message = '')
+    {
+        $cards = [
+            'any' => '(^\d+$)',
+            'visa' => '(^4[0-9]{12}(?:[0-9]{3})?$)',
+            'mastercard' => '(^5[1-5][0-9]{14}$)',
+            'americanexpress' => '(^3[47][0-9]{13}$)',
+            'dinersclub' => '(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)',
+            'discover' => '(^6(?:011|5[0-9]{2})[0-9]{12}$)',
+            'jcb' => '(^(?:2131|1800|35\d{3})\d{11}$)'
+        ];
+        $allowed = $cards;
+        if (is_array($types)) {
+            $allowed = [];
+            foreach ($types as $type) {
+                if (!isset($cards[$type])) {
+                    $allowed[$type] = $cards[$type];
+                }
+            }
+        }
+        return $this->mod10($message)->callback(function ($value, $data) use ($allowed) {
+            foreach ($allowed as $card) {
+                if (preg_match($card, $value)) {
+                    return true;
+                }
+            }
+            return false;
+        }, $message);
+    }
+    /**
+     * Add an IBAN validation
+     * @method iban
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function iban($message)
+    {
+        return $this->callback(function ($value, $data) {
+            $value = str_replace([' ','-'], '', strtolower($value));
+            if (!preg_match('(^[a-z0-9]{5,}$)', $value)) {
+                return false;
+            }
+            $chars = [
+                'a'=>10,'b'=>11,'c'=>12,'d'=>13,'e'=>14,'f'=>15,'g'=>16,'h'=>17,'i'=>18,'j'=>19,'k'=>20,'l'=>21,'m'=>22,
+                'n'=>23,'o'=>24,'p'=>25,'q'=>26,'r'=>27,'s'=>28,'t'=>29,'u'=>30,'v'=>31,'w'=>32,'x'=>33,'y'=>34,'z'=>35
+            ];
+            $iban = substr($value, 4) . substr($value, 0, 4);
+            $iban = str_replace(array_keys($chars), array_values($chars), $iban);
+
+            $mod = "";
+            do {
+                $new  = $mod . substr($iban, 0, 5);
+                $iban = substr($iban, 5);
+                $mod  = $new % 97;
+            } while (strlen($iban));
+            
+            return $mod === 1;
+        }, $message);
+    }
+    /**
+     * Add an UUID validation
+     * @method uuid
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function uuid($message)
+    {
+        return $this->regex('(^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$)i', $message);
+    }
+    /**
+     * Add a MAC validation
+     * @method mac
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function mac($message)
+    {
+        return $this->regex('(^(([0-9a-fA-F]{2}-){5}|([0-9a-fA-F]{2}:){5})[0-9a-fA-F]{2}$)', $message);
+    }
+    /**
+     * Add a Bulgarian EGN validation
+     * @method bgEGN
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function bgEGN($message)
+    {
+        return $this->callback(function ($value, $data) {
+            if (!ctype_digit($value) || strlen($value) !== 10) {
+                return false;
+            }
+            $year  = substr($value, 0, 2);
+            $month = substr($value, 2, 2);
+            $day   = substr($value, 4, 2);
+            if ($month > 40) {
+                $month -= 40;
+                $year  += 2000;
+            } elseif ($month > 20) {
+                $month -= 20;
+                $year  += 1800;
+            } else {
+                $year  += 1800;
+            }
+            if (!checkdate($month, $day, $year)) {
+                return false;
+            }
+
+            $value = str_split($value);
+            $check = array_pop($value);
+            $weights = [ 2, 4, 8, 5, 10, 9, 7, 3, 6 ];
+            foreach ($value as $k => $v) {
+                $value[$k] = $v * $weights[$k];
+            }
+            return (array_sum($value) % 11) % 10 === (int)$check;
+        }, $message);
+    }
+    /**
+     * Add a Bulgarian LNC validation
+     * @method bgEGN
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function bgLNC($message)
+    {
+        return $this->callback(function ($value, $data) {
+            if (!ctype_digit($value) || strlen($value) !== 10) {
+                return false;
+            }
+            $value = str_split($value);
+            $check = array_pop($value);
+            $weights = [ 21, 19, 17, 13, 11, 9, 7, 3, 1 ];
+            foreach ($value as $k => $v) {
+                $value[$k] = $v * $weights[$k];
+            }
+            return array_sum($value) % 10 === (int)$check;
+        }, $message);
+    }
+    /**
+     * Add a Bulgarian identification number validation (EGN or LNC)
+     * @method bgIDN
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function bgIDN($message)
+    {
+        return $this->callback(function ($value, $data) {
+            if (!ctype_digit($value) || strlen($value) !== 10) {
+                return false;
+            }
+            $value = str_split($value);
+            $check = array_pop($value);
+
+            $weights = [ 21, 19, 17, 13, 11, 9, 7, 3, 1 ];
+            $sum = 0;
+            foreach ($value as $k => $v) {
+                $sum += $v * $weights[$k];
+            }
+            if ($sum % 10 === (int)$check) {
+                return true;
+            }
+
+            $year  = substr($value, 0, 2);
+            $month = substr($value, 2, 2);
+            $day   = substr($value, 4, 2);
+            if ($month > 40) {
+                $month -= 40;
+                $year  += 2000;
+            } elseif ($month > 20) {
+                $month -= 20;
+                $year  += 1800;
+            } else {
+                $year  += 1800;
+            }
+            if (!checkdate($month, $day, $year)) {
+                return false;
+            }
+
+            $value = str_split($value);
+            $check = array_pop($value);
+            $weights = [ 2, 4, 8, 5, 10, 9, 7, 3, 6 ];
+            foreach ($value as $k => $v) {
+                $value[$k] = $v * $weights[$k];
+            }
+            return (array_sum($value) % 11) % 10 === (int)$check;
+        }, $message);
+    }
+    /**
+     * Add a Bulgarian male EGN validation
+     * @method bgMaleEGN
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function bgMaleEGN($message)
+    {
+        return $this->bgEGN($message)->callback(function ($value, $data) {
+            return substr($value, 8, 1) % 2 === 0;
+        }, $message);
+    }
+    /**
+     * Add a Bulgarian female EGN validation
+     * @method bgFemaleEGN
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function bgFemaleEGN($message)
+    {
+        return $this->bgEGN($message)->callback(function ($value, $data) {
+            return substr($value, 8, 1) % 2 === 1;
+        }, $message);
+    }
+    /**
+     * Add a Bulgarian BULSTAT validation
+     * @method bgBulstat
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function bgBulstat($message)
+    {
+        return $this->callback(function ($value, $data) {
+            $value = preg_replace('(^BG)', '', $value);
+            if (!ctype_digit($value) || !in_array(strlen($value), [ 9, 13 ])) {
+                return false;
+            }
+            $value = str_split($value);
+            $sum = 0;
+            for ($i = 0; $i < 8; $i++) {
+                $sum += $value[$i] * ($i + 1);
+            }
+            $mod = $sum % 11;
+            if ($mod === 10) {
+                $sum = 0;
+                for ($i = 0; $i < 8; $i++) {
+                    $sum += $value[$i] * ($i + 3);
+                }
+                $mod = ($sum % 11) % 10;
+            }
+            if ((int)$value[8] !== $mod) {
+                return false;
+            }
+            if (isset($value[9])) {
+                $sum = $value[8] * 2 + $value[9] * 7 + $value[10] * 3 + $value[11] * 5;
+                $mod = $sum % 11;
+                if ($mod === 10) {
+                    $sum = $value[8] * 4 + $value[9] * 9 + $value[10] * 5 + $value[11] * 7;
+                    $mod = ($sum % 11) % 10;
+                }
+                if ((int)$value[12] !== $mod) {
+                    return false;
+                }
+            }
+            return true;
+        }, $message);
+    }
+    /**
+     * Add a Bulgarian name validation
+     * @method bgName
+     * @param  string     $message an optional message to include in the report if the validation fails
+     * @return self
+     */
+    public function bgName($message)
+    {
+        return $this->regex('(^([А-Я][a-я]*[ -]*){2,}$)', $message);
+    }
 }
